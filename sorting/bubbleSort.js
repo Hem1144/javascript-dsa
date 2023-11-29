702f1ee3b0d0d742cc437169118f51590525d593
Hemlal Dulal <hemlaldulal.986@gmail.com>
2023-11-29 08:53:26 +0545

    feat: Did insertion sort problem

---
+++ sorting/bubbleSort.js
@@ -0,0 +1,18 @@
+//! Bubble sort
+function bubbleSort(arr) {
+  const n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // swap arr[j] and arr[j+1]
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+
+  return arr;
+}
+let sort = bubbleSort([0, 2, 4, 6, 8, -5, -4, -3, -2]);
+
+console.log(sort);
---
+++ sorting/insertionSort.js
@@ -0,0 +1,15 @@
+const sorted = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let curr = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > curr) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = curr;
+  }
+  return arr;
+};
+
+console.log(sorted([5, 4, 3, 2, 1]));
