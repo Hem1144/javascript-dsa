d6606c00b4d47710186884b0670880d0affc131b
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-01-08 13:25:22 +0545

    feat: Completed leetcode problem related to best time to buy and sell stock

---
+++ array/bestTimeToBuyAndSell.js
@@ -0,0 +1,14 @@
+function maxProfit(prices) {
+    let maxProfit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] > prices[i - 1]) {
+            maxProfit += prices[i] - prices[i - 1];
+        }
+    }
+
+    return maxProfit;
+}
+
+const price=[7, 1, 5, 3, 6, 4]
+console.log(maxProfit(price))
