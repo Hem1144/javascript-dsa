c4783ebcb2970e42845c9a5857e6470f3bb45b15
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-01-09 15:14:06 +0545

    feat: Alternative solution for reverseInteger

--- string/reverseInteger.js
+++ string/reverseInteger.js
@@ -1,8 +1,20 @@
-const reverseInteger = (num) => {
-  let reverseNum = num.toString().split("").reverse().join("");
-  if (reverseNum.endsWith("-") || reverseNum.endsWith(0)) {
-    reverseNum = "-" + reverseNum;
-    return parseInt(reverseNum);
+const reverseInteger = (x) => {
+  if (x === 0) {
+    return 0;
+  }
+
+  const limit = 2 ** 31;
+
+  let reverseNum = x.toString().split("").reverse().join("");
+
+  if (reverseNum.endsWith("-")) {
+    reverseNum = "-" + reverseNum.slice(0, -1);
+  } else if (reverseNum.endsWith("0")) {
+    reverseNum = reverseNum.slice(0, -1);
+  }
+
+  if (reverseNum > limit - 1 || reverseNum < -limit) {
+    return 0;
   } else {
     return parseInt(reverseNum);
   }
