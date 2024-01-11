f20b8cb1c0a31a9131fdae0971df5af18a9bcf10
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-01-11 22:59:19 +0545

    feat: Completed callbackFunction code

---
+++ interview/callbackFunction.js
@@ -0,0 +1,14 @@
+// setTimeout(() => {
+//   console.log("Timer function is executed");
+// }, 2000);
+
+// function x(y) {
+//   console.log("x");
+//   y();
+// }
+
+// x(function y() {
+//   console.log("y");
+// });
+
+console.log("Start");
