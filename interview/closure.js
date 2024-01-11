4d0486ba7753f0683166402734cfbc5a8ce01648
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-01-11 22:58:08 +0545

    feat: Added example code with logic of closure

--- interview/closure.js
+++ interview/closure.js
@@ -29,18 +29,45 @@
 // const myFunc = makeFunc();
 // myFunc();
 
-function callCount() {
-  let count = 0;
-  return function () {
-    return (count += 1);
-  };
+// function callCount() {
+//   let count = 0;
+//   return function () {
+//     return (count += 1);
+//   };
+// }
+
+// let newFunction1 = callCount();
+// let newFunction2 = callCount();
+
+// console.log(newFunction1());
+// console.log(newFunction1());
+
+// console.log(newFunction2());
+// console.log(newFunction2());
+
+// function x() {
+//   const a = 10;
+//   function y() {
+//     console.log(a);
+//   }
+//   y();
+// }
+// x();
+
+function outerFunction(outerVariable) {
+  //* Inner function defined within the outer function
+  function innerFunction(innerVariable) {
+    //* Accesses both outerVariable and innerVariable
+    console.log("Outer Variable:", outerVariable);
+    console.log("Inner Variable:", innerVariable);
+  }
+
+  // Returning the inner function, creating a closure
+  return innerFunction;
 }
 
-let newFunction1 = callCount();
-let newFunction2 = callCount();
+//* Creating a closure by calling outerFunction with a specific value
+let closureExample = outerFunction("I am from the outer function");
 
-console.log(newFunction1());
-console.log(newFunction1());
-
-console.log(newFunction2());
-console.log(newFunction2());
+//* Using the closure to access the inner function
+closureExample("I am from the inner function");
