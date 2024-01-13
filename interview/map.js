761be5ba1e66082068253c7cd3154471973acffb
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-01-13 14:43:47 +0545

    feat: Added code for map() function

---
+++ interview/map.js
@@ -0,0 +1,13 @@
+//* way to use map function
+const arr = [5, 1, 3, 2, 10];
+
+function double(x) {
+  return x * 2;
+}
+function triple(x) {
+  return x * 3;
+}
+
+const output = arr.map((x) => x.toString(2));
+
+console.log(output);
