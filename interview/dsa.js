c0a9dcf09fcb1c1667b2042a21ee50198ba768b6
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-01-16 22:40:11 +0545

    feat: Added different logic to get fibonacci series

--- interview/dsa.js
+++ interview/dsa.js
@@ -4,3 +4,29 @@
 // };
 // const result = isPalindrome(1210);
 // console.log(result);
+
+//! Fibonacci Number
+//* first method
+// const fibo = function (n) {
+//   const arr = [0, 1];
+
+//   for (let i = 2; i <= n; i++) {
+//     arr.push(arr[i - 1] + arr[i - 2]);
+//   }
+//   console.log(arr);
+// };
+// fibo(8);
+
+//* second method using recursion
+// const fibo = function (n) {
+//   if (n <= 1) return n;
+
+//   return fibo(n - 1) + fibo(n - 2);
+// };
+// const result = fibo(3);
+// console.log(result);
+
+//! third method
+const fibo = (n) => (n <= 1 ? n : fibo(n - 1) + fibo(n - 2));
+
+console.log(fibo(4));
