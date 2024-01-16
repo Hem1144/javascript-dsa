fbf9425a0c2027a6bc6edf3bafe4bf58b6a8b06b
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-01-16 14:18:25 +0545

    feat: Added questions related to an object

--- interview/object.js
+++ interview/object.js
@@ -63,6 +63,104 @@
 // a[c] = 456;
 // console.log(a[b]);
 
+//! What's  JSON.stringify() and JSON.parse()
+// const user = {
+//   name: "Hemlal Dulal",
+//   age: 25,
+// };
+// const strObj = JSON.stringify(user); //* convert javascript object into string
+// localStorage.setItem("test", strObj);
+
+// console.log(JSON.parse(localStorage.getItem("test")));
+// const objStr = JSON.parse(strObj); //* convert javascript string into javascript object
+// console.log(objStr);
+
+//! Question-5 What's the output
+// console.log([[..."Hemlal"]]);
+// const user = { name: "Hemlal", age: 25 };
+// const admin = { admin: true, ...user };
+// console.log(admin);
+
+//! Question-6 What's the output
+// const settings = {
+//   username: "Hemlal",
+//   level: 19,
+//   health: 95,
+// };
+// const data = JSON.stringify(settings, ["level", "health"]); //* only stringify level and health
+// console.log(data);
+
+//! Question-7 What's the output
+// const shape = {
+//   radius: 10,
+//   diameter() {
+//     return this.radius * 2;
+//   },
+//   perimeter: () => 2 * Math.PI * this.radius, //* arrow function takes as an windows object
+// };
+// console.log(shape.diameter());
+// console.log(shape.perimeter());
+
+//! Question-8 What is destructuring in objects ?
+// const user = {
+//   name: "Hemlal",
+//   age: 25,
+//   address: {
+//     city: "Melamchi",
+//     state: "Bagmati",
+//   },
+// };
+// const {
+//   name,
+//   address: { city, state },
+// } = user;
+// console.log(city);
+
+//! Question-9 What's the output
+// function getItems(fruitList, favoriteFruit, ...args) {
+//   //* rest parameter always must be in at last
+//   return [...fruitList, ...args, favoriteFruit];
+// }
+// console.log(getItems(["banana", "apple"], "pear", "orange"));
+
+//! Question-10 What's the output
+// let c = { greeting: "Hey!" };
+// let d;
+// d = c; //* assign reference of variable c into d
+// c.greeting = "Hello";
+// console.log(d.greeting);
+
+//! Question-11 What's the output
+// console.log({ a: 1 } == { a: 1 }); //* it compares the object referance not an value
+// console.log({ a: 1 } === { a: 1 });
+
+//! Question-12 What's the output
+// let person = { name: "Hemlal" };
+// const members = [person];
+// person = null;
+// console.log(members);
+
+//! Question-13 What's the output
+// const value = { number: 10 };
+// const multiply = (x = { ...value }) => {
+//   console.log((x.number *= 2));
+// };
+// multiply();
+// multiply();
+// multiply(value);
+// multiply(value);
+
+//! shallow copy and deep copy
+// let user = {
+//   name: "Hemlal Dulal",
+//   age: 25,
+// };
+// const objClone = Object.assign({}, user); //* 1st way
+// const objClone = JSON.parse(JSON.stringify(user)); //* 2nd way
+// const objClone = { ...user }; //* 3rd way
+// objClone.name = "Dulal";
+// console.log(user, objClone);
+
 //! IFFE
 // const func = (function (a) {
 //   delete a; //! delete only delete the object key-value not a local variable
