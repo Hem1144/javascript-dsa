d5452a6afae95469876c5d70fbe4fc1223a2ab54
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-01-16 21:00:47 +0545

    feat: Solved problem of palindrome number

---
+++ interview/dsa.js
@@ -0,0 +1,6 @@
+//! An integer is a palindrome when it reads the same forward and backend
+// const isPalindrome = function (x) {
+//   return x < 0 ? false : x === +x.toString().split("").reverse().join(""); //* convert it into string
+// };
+// const result = isPalindrome(1210);
+// console.log(result);
