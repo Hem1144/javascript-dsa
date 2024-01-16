db605ba5710cbd72524769e6f1c0ec0491e8d860
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-01-16 23:05:54 +0545

    feat: Solution code to identify palindrome or not using hashMap method

--- interview/dsa.js
+++ interview/dsa.js
@@ -31,9 +31,27 @@
 // console.log(fibo(4));
 
 //! Valid Anagram
-const isAnagram = function (s, t) {
-  s = s.split("").sort().join("");
-  t = t.split("").sort().join("");
-  return s === t;
+// const isAnagram = function (s, t) {
+//   s = s.split("").sort().join("");
+//   t = t.split("").sort().join("");
+//   return s === t;
+// };
+// console.log(isAnagram("nagaram", "anagram"));
+
+//* second method
+const isAnagram = (s, t) => {
+  if (s.length !== t.length) return false;
+
+  let obj1 = {};
+  let obj2 = {};
+
+  for (let i = 0; i < s.length; i++) {
+    obj1[s[i]] = (obj1[s[i]] || 0) + 1;
+    obj2[t[i]] = (obj2[t[i]] || 0) + 1;
+  }
+  for (const key in obj1) {
+    if (obj1[key] !== obj2[key]) return false;
+  }
+  return true;
 };
 console.log(isAnagram("nagaram", "anagram"));
