7d83b0a8ce838db734d33bdc42e3264fe244b4e8
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-01-17 14:36:09 +0545

    feat: Add a new node at the end of the node

---
+++ interview/addNodeInLinkedList.js
@@ -0,0 +1,53 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  //! Function to insert a node at the end of the linked list
+  append(data) {
+    const newNode = new Node(data);
+
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+
+    current.next = newNode;
+  }
+
+  //! Function to display the linked list
+  display() {
+    let current = this.head;
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+}
+
+// Example usage:
+const linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+
+console.log("Original linked list:");
+linkedList.display();
+
+// Insert a new node at the end
+linkedList.append(4);
+
+console.log("\nLinked list after inserting a new node at the end:");
+linkedList.display();
