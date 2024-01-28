33c4ae013dfe5ca57298fb2c03918dcab063ddf0
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-01-28 23:09:08 +0545

    feat: Completed Insert Interval array problem, Leetcode question 57

---
+++ array/insertInterval.js
@@ -0,0 +1,30 @@
+//* Leetcode question number 57
+
+const insertInterval = (intervals, newInterval) => {
+  const result = [];
+
+  for (let interval of intervals) {
+    if (interval[1] < newInterval[0]) {
+      result.push(interval);
+    } else if (interval[0] > newInterval[1]) {
+      result.push(newInterval);
+      newInterval = interval;
+    } else {
+      newInterval[0] = Math.min(interval[0], newInterval[0]);
+      newInterval[1] = Math.max(interval[1], newInterval[1]);
+    }
+  }
+  result.push(newInterval);
+  return result;
+};
+
+let intervals = [
+  [1, 2],
+  [3, 5],
+  [6, 7],
+  [8, 10],
+  [12, 16],
+];
+
+let newInterval = [4, 8];
+console.log(insertInterval(intervals, newInterval));
